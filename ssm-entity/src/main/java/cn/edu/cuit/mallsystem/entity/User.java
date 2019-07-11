5e6e51a89c5154207c8d5d4bf2eb891f7c57edee
Hedroins <hejialin1314@outlook.com>
2019-07-11 16:02:03 +0800

    创建了User实体类

---
+++ ssm-entity/src/main/java/cn/edu/cuit/mallsystem/entity/User.java
@@ -0,0 +1,62 @@
+package cn.edu.cuit.mallsystem.entity;
+
+public class User {
+	private int id;
+	private String name;
+	private String pass;
+	private String address;
+	private String phone;
+
+	public int getId() {
+		return id;
+	}
+
+	public User() {
+
+	}
+
+	public User(String name, String pass, String address, String phone) {
+		super();
+		this.name = name;
+		this.pass = pass;
+		this.address = address;
+		this.phone = phone;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+}
